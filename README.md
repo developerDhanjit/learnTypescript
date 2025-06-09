a3f234759a3330b17eb0b31e4b71210153b36652
Dhanjit Nath <dhanjitxnath@gmail.com>
2025-06-09 16:45:33 +0100

    learn about any vid 6

--- README.md
+++ README.md
@@ -11,4 +11,7 @@ STATIC CHECKING !
 TS is a developement tool
 Project runs  JS !
 
+-> learning type inference no overusing colons , ts is smart ! 
+-> any is just a get away avoid it ! it just tells ts to avoid typechecking it 
+   noImplicitany
 ```
--- basics/variables.ts
+++ basics/variables.ts
@@ -4,5 +4,23 @@ let myNum : number = 69 ;
 userName.toUpperCase()
 myNum.toLocaleString()
 
+// boolean 
+
+let isLoogedIn: boolean = false
+
+//  any 
+
+// let youtuber ; => ts infer this as any 
+
+let youtuber: string ; 
+// use this instead
+
+function getYoutuber(){
+    return "hc"
+}
+
+youtuber = getYoutuber();
+
 console.log(userName, myNum);
+console.log(youtuber);
 export {}
