24effe80bb45d8b4ecc7bf018a483f10829dd915
Dhanjit Nath <dhanjitxnath@gmail.com>
2025-06-16 03:54:19 +0100

    learnt about tupules and their bad behaviour

---
+++ basics/tupules.ts
@@ -0,0 +1,10 @@
+// Tupules : ordering the items inside the array 
+
+let NewUser : [string, number, boolean];
+
+NewUser = ["dhanjit", 101, true]
+// NewUser = ["dhanjit", "101", true] not allowed 
+
+let rgb: [number, number, number]
+
+rgb = [244,255,255]
